e8c6313b5f5d6c3cb1798fd7dd90cbbaa2146c57
Jonathan Lemos <jonlemofficial@hotmail.com>
2026-01-17 21:55:06 -0400

    Removing  field from default profiles.

--- src/lib/profiles.ts
+++ src/lib/profiles.ts
@@ -5,18 +5,15 @@ export const profiles: Omit<User, 'id'>[] = [
     key: 1,
     username: 'jonathan',
     fullname: 'Jonathan Lemos',
-    email: 'jonathan@example.com',
   },
   {
     key: 2,
     username: 'mileidy',
     fullname: 'Mileidy Perez',
-    email: 'kira@example.com',
   },
   {
     key: 3,
     username: 'marc',
     fullname: 'Marc Hudson',
-    email: 'marc@example.com'
   },
 ];
